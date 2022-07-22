995517b03346872ad8c1a1b4b0b74a77e6bb1d2f
Tyler <tillerwang@gmail.com>
2022-07-22 13:59:57 -0400

    7 - 21 - 2022

---
+++ Library_Exercise.py
@@ -0,0 +1,18 @@
+import math
+import random
+
+a = math.pow(2,4)
+print(a)
+
+b = math.sqrt(16)
+print(b)
+
+c = random.randint(0,100)
+print(c)
+
+names = ["Jack", "Joseph", "Amelia"]
+
+random.shuffle(names)
+
+d = random.choice(names)
+print(d)
