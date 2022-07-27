6abd8a04d4a816f7fffa8bfb89d238dc9297f4da
Tyler <tillerwang@gmail.com>
2022-07-27 11:44:51 -0400

    7 - 22 - 2022

--- Social_Network_Assignment_Folder.py/Social_Network_Assignment.py
+++ Social_Network_Assignment_Folder.py/Social_Network_Assignment.py
@@ -5,7 +5,7 @@ name_file.writelines(name)
 name_file.close()
 description = input("Description: ")
 
-friends = []
+friendsList = []
 
 people = ["Jack", "Will", "Josh", "Lauren", "Emily", "Sophia"]
 
@@ -33,15 +33,15 @@ messagesToSophiaFromMe = []
 
 def acceptFriendRequest(x):
     friendRequestsToMe.remove(x)
-    friends.append(x)
+    friendsList.append(x)
 
 def declineFriendRequest(x):
     friendRequestsToMe.remove(x)
 
 def friends():
-    friendsAction = input("1. View Friends\n2. Add Friends\n3. View Friend Requests\n")
+    friendsAction = input("1. View Friends\n2. Add Friends\n3. View Friend Requests\n4. Go Back\n")
     if friendsAction == "1":
-        print(friends)
+        print(friendsList)
         friends()
     elif friendsAction == "2":
         print(people)
@@ -51,25 +51,18 @@ def friends():
         x.close()
         if friendRequest == "Jack":
             friendRequestsToJack.append(y)
-            #addFriendsGoBack()
         elif friendRequest == "Will":
             friendRequestsToWill.append(y)
-            #addFriendsGoBack()
         elif friendRequest == "Josh":
             friendRequestsToJosh.append(y)
-            #addFriendsGoBack()
         elif friendRequest == "Lauren":
             friendRequestsToLauren.append(y)
-            #addFriendsGoBack()
         elif friendRequest == "Emily":
             friendRequestsToEmily.append(y)
-            #addFriendsGoBack()
         elif friendRequest == "Sophia":
             friendRequestsToSophia.append(y)
-            #addFriendsGoBack()
         else:
             invalidInput = input("Invalid input, enter again.\n")
-            #addFriendsGoBack()
         friends()
     elif friendsAction == "3":
         print(friendRequestsToMe)
@@ -78,12 +71,93 @@ def friends():
         declineFriend = input("Choose who's friend request to decline.\n")
         declineFriendRequest(declineFriend)
         friends()
+    elif friendsAction == "4":
+        socialNetwork()
     else:
         invalidInput = input("Invalid input, enter again.\n")
-        friends()
-    socialNetworkActions()
 
-def socialNetworkActions():
+def message():
+    print(friendsList)
+    messageFriend = input("Enter who to message.\n")
+    sendMessage = input("Enter message.\n")
+    if messageFriend == "Jack":
+        print("\nMy Chat With Jack")
+        i = 0
+        x = len(messagesToMeFromJack)
+        while i < x:
+            print("Jack:\n" + messagesToMeFromJack[i] + "\n")
+            i += 1
+        messagesToJackFromMe.append(sendMessage)
+        y = len(messagesToJackFromMe)
+        while i < y:
+            print("Me:\n" + messagesToJackFromMe[i] + "\n")
+            i += 1
+    elif messageFriend == "Will":
+        print("\nMy Chat With Will")
+        i = 0
+        x = len(messagesToMeFromWill)
+        while i < x:
+            print("Will:\n" + messagesToMeFromWill[i] + "\n")
+            i += 1
+        messagesToWillFromMe.append(sendMessage)
+        y = len(messagesToWillFromMe)
+        while i < y:
+            print("Me:\n" + messagesToWillFromMe[i] + "\n")
+            i += 1
+    elif messageFriend == "Josh":
+        print("\nMy Chat With Josh")
+        i = 0
+        x = len(messagesToMeFromJosh)
+        while i < x:
+            print("Josh:\n" + messagesToMeFromJosh[i] + "\n")
+            i += 1
+        messagesToJoshFromMe.append(sendMessage)
+        y = len(messagesToJoshFromMe)
+        while i < y:
+            print("Me:\n" + messagesToJoshFromMe[i] + "\n")
+            i += 1
+    elif messageFriend == "Lauren":
+        print("\nMy Chat With Lauren")
+        i = 0
+        x = len(messagesToMeFromLauren)
+        while i < x:
+            print("Lauren:\n" + messagesToMeFromLauren[i] + "\n")
+            i += 1
+        messagesToLaurenFromMe.append(sendMessage)
+        y = len(messagesToLaurenFromMe)
+        while i < y:
+            print("Me:\n" + messagesToLaurenFromMe[i] + "\n")
+            i += 1
+    elif messageFriend == "Emily":
+        print("\nMy Chat With Emily")
+        i = 0
+        x = len(messagesToMeFromEmily)
+        while i < x:
+            print("Emily:\n" + messagesToMeFromEmily[i] + "\n")
+            i += 1
+        messagesToEmilyFromMe.append(sendMessage)
+        y = len(messagesToEmilyFromMe)
+        while i < y:
+            print("Me:\n" + messagesToEmilyFromMe[i] + "\n")
+            i += 1
+    elif messageFriend == "Sophia":
+        print("\nMy Chat With Sophia")
+        i = 0
+        x = len(messagesToMeFromSophia)
+        while i < x:
+            print("Sophia:\n" + messagesToMeFromSophia[i] + "\n")
+            i += 1
+        messagesToSophiaFromMe.append(sendMessage)
+        y = len(messagesToSophiaFromMe)
+        while i < y:
+            print("Me:\n" + messagesToSophiaFromMe[i] + "\n")
+            i += 1
+    else:
+        print(messageFriend + " is not a person.")
+        message()
+    socialNetwork()
+
+def socialNetwork():
     action = input("1. Edit Profile\n2. Friends\n3. Message\nEnter a number to choose what to do.\n")
     if action == "1":
         editName = input("Name: ")
@@ -91,97 +165,13 @@ def socialNetworkActions():
         name_file.writelines(editName)
         name_file.close()
         editDescription = input("Description: ")
-        socialNetworkActions()
+        socialNetwork()
     elif action == "2":
         friends()
     elif action == "3":
-        print(friends)
-        messageFriend = input("Enter who to message.\n")
-        message = input("Enter message.\n")
-        if messageFriend == "Jack":
-            print("\nMy Chat With Jack")
-            i = 0
-            x = len(messagesToMeFromJack)
-            while i < x:
-                print("\nJack:\n" + messagesToMeFromJack[i] + "\n")
-                i += 1
-            messagesToJackFromMe.append(message)
-            y = len(messagesToJackFromMe)
-            while i < y:
-                print("\nMe:\n" + messagesToJackFromMe[i] + "\n")
-                i += 1
-            #messageGoBack()
-        elif messageFriend == "Will":
-            print("\nMy Chat With Will")
-            i = 0
-            x = len(messagesToMeFromWill)
-            while i < x:
-                print("\nWill:\n" + messagesToMeFromWill[i] + "\n")
-                i += 1
-            messagesToWillFromMe.append(message)
-            y = len(messagesToWillFromMe)
-            while i < y:
-                print("\nMe:\n" + messagesToWillFromMe[i] + "\n")
-                i += 1
-            #messageGoBack()
-        elif messageFriend == "Josh":
-            print("\nMy Chat With Josh")
-            i = 0
-            x = len(messagesToMeFromJosh)
-            while i < x:
-                print("\nJosh:\n" + messagesToMeFromJosh[i] + "\n")
-                i += 1
-            messagesToJoshFromMe.append(message)
-            y = len(messagesToJoshFromMe)
-            while i < y:
-                print("\nMe:\n" + messagesToJoshFromMe[i] + "\n")
-                i += 1
-            #messageGoBack()
-        elif messageFriend == "Lauren":
-            print("\nMy Chat With Lauren")
-            i = 0
-            x = len(messagesToMeFromLauren)
-            while i < x:
-                print("\nLauren:\n" + messagesToMeFromLauren[i] + "\n")
-                i += 1
-            messagesToLaurenFromMe.append(message)
-            y = len(messagesToLaurenFromMe)
-            while i < y:
-                print("\nMe:\n" + messagesToLaurenFromMe[i] + "\n")
-                i += 1
-            #messageGoBack()
-        elif messageFriend == "Emily":
-            print("\nMy Chat With Emily")
-            i = 0
-            x = len(messagesToMeFromEmily)
-            while i < x:
-                print("\nEmily:\n" + messagesToMeFromEmily[i] + "\n")
-                i += 1
-            messagesToEmilyFromMe.append(message)
-            y = len(messagesToEmilyFromMe)
-            while i < y:
-                print("\nMe:\n" + messagesToEmilyFromMe[i] + "\n")
-                i += 1
-            #messageGoBack()
-        elif messageFriend == "Sophia":
-            print("\nMy Chat With Sophia")
-            i = 0
-            x = len(messagesToMeFromSophia)
-            while i < x:
-                print("\nSophia:\n" + messagesToMeFromSophia[i] + "\n")
-                i += 1
-            messagesToSophiaFromMe.append(message)
-            y = len(messagesToSophiaFromMe)
-            while i < y:
-                print("\nMe:\n" + messagesToSophiaFromMe[i] + "\n")
-                i += 1
-            #messageGoBack()
-        else:
-            invalidInput = input("Invalid input, enter again.\n")
-            #messageGoBack()
-        socialNetworkActions()
+        message()
     else:
-        invalidInput = input("Invalid input, enter again.\n")
-        socialNetworkActions()
+        print("Invalid input, enter again.")
+        socialNetwork()
 
-socialNetworkActions()
+socialNetwork()
--- Social_Network_Assignment_Folder.py/experiment.py
+++ Social_Network_Assignment_Folder.py/experiment.py
@@ -1,4 +1,4 @@
-#Experimenting if x can be used as a translator for the list name:
+#Experimenting if x can be used as a translator for a list of people who sent someone a friend request:
 #elif action == "2":
 #    friendsAction = input("1. View Friends\n2. Add Friends\n3. View Friend Requests\n")
 #    if friendsAction == "1":
@@ -8,18 +8,18 @@
 #        print(people)
 #        friendRequest = input("Send friend request to: ")
 
-        #Does Not Work: (x cannot be used as a translator for the list name)
-        #x = "friendRequestsTo" + friendRequest
-        #x = []
-        #x.append("name")
-        #print(x)
-        #print(friendRequestsToJack)
+#        Does Not Work: Variables cannot be used as a translator for a list name.
+#        x = "friendRequestsTo" + friendRequest
+#        x = []
+#        x.append("name")
+#        print(x)
+#        print(friendRequestsToJack)
 
-        #Does Not Work: (x cannot be used as a translator for the list name)
-        #x = "friendRequestsTo" + friendRequest
-        #print(x)
-        #x.append("name")
-        #print("friendRequestsTo" + friendRequest)
+#        Does Not Work: Variables cannot be used as a translator for a list name.
+#        x = "friendRequestsTo" + friendRequest
+#        print(x)
+#        x.append("name")
+#        print("friendRequestsTo" + friendRequest)
 
 #        friendsGoBack()
 #    elif friendsAction == "3":
@@ -28,3 +28,19 @@
 #    else:
 #        invalidInput = input("Invalid input, enter again.")
 #    goBack()
+
+#Experimenting if you can count how many elements of a certain name are in the friends list:
+#friends = []
+
+#Does Not Work: You cannot count how many elements of a specific value are in a list.
+#def notInFriendsList():
+#    x = friends.count(messageFriend)
+#    if x == 0:
+#        print(messageFriend + " is not in your friends list.")
+#        messageFriend = input("Enter who to message.\n")
+#        notInFriendsList()
+
+#elif action == "3":
+#    print(friends)
+#    messageFriend = input("Enter who to message.\n")
+#    notInFriendsList()
